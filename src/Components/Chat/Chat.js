2852de7506a8b834c1999c19fe923b604b104a56
Madhav Anand <madhav1004@icloud.com>
2020-10-19 22:08:17 +0530

    done

--- src/Components/Chat/Chat.js
+++ src/Components/Chat/Chat.js
@@ -4,6 +4,8 @@ import ChatInput from "../ChatInput/ChatInput";
 import {useHistory} from "react-router-dom";
 import {useStateValue} from "../../StateProvider";
 import speak from "../../speak";
+import {Button} from "@material-ui/core";
+import {auth} from "../../firebase";
 
 function Chat() {
     const history = useHistory();
@@ -18,9 +20,14 @@ function Chat() {
     },[message])
 
 
+    const signOut = ()=>{
+        auth.signOut();
+    }
+
     return (
         <div className="chat">
             <ChatInput />
+            <Button onClick={signOut} className={'signout-button submit-button'}>Sign Out</Button>
         </div>
     )
 }
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -21,6 +21,7 @@ function Home() {
             .catch(error=>{
                 alert(error.message)
             })
+
     }
 
     useEffect(()=>{
